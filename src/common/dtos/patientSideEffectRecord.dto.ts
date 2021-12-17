4fef2acf7ceeca3760251565ce27da608b0c35d0
Brett Larry Onions <46473797+brett-onions@users.noreply.github.com>
2021-12-17 19:00:15 +0200

    renamed to createafiedto

--- src/common/dtos/patientSideEffectRecord.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
-
-export class PatientSideEffectRecordDto {
-	@IsNotEmpty()
-	patientSelectedSideEffects: string;
-	@IsOptional()
-	@IsNotEmpty()
-	patientOtherSideEffects: string;
-}
