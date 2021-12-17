ef2081e14e2a7c02b69c2c51c03511c18b8aefce
Brett Larry Onions <46473797+brett-onions@users.noreply.github.com>
2021-12-17 18:59:44 +0200

    removing unneeded module

--- src/second-dosage-reminder/second-dosage-reminder.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('second-dosage-reminder')
-export class SecondDosageReminderController {}
--- src/second-dosage-reminder/second-dosage-reminder.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SecondDosageReminderService } from './second-dosage-reminder.service';
-import { SecondDosageReminderController } from './second-dosage-reminder.controller';
-
-@Module({
-  providers: [SecondDosageReminderService],
-  controllers: [SecondDosageReminderController]
-})
-export class SecondDosageReminderModule {}
