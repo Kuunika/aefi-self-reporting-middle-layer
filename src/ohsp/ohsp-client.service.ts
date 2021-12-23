735d8c7563e29e3d3735445bc723f591439112f5
Brett Larry Onions <46473797+brett-onions@users.noreply.github.com>
2021-12-23 18:28:19 +0200

    adding number of dosage for vaccine

--- src/ohsp/ohsp-client.service.ts
+++ src/ohsp/ohsp-client.service.ts
@@ -1,10 +1,11 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, ServiceUnavailableException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { lastValueFrom } from 'rxjs';
-import { Dhis2Option, Dhis2OptionsSet, OrgUnit } from '../common/types';
-import { IDhis2TrackedEntityInstance } from '../common/types/dhis2-tracked-entity-instance';
-
+import { CreateNewDhis2EventDto } from '../common/dtos/create-new-dhis2-event.dto';
+import { CreateAefiDto } from '../common/dtos/create-aefi.dto';
+import { Dhis2Option, Dhis2OptionsSet, OrgUnit, IDhis2TrackedEntityInstance, Dhis2DataElement } from '../common/types';
+import moment from 'moment';
 @Injectable()
 export class OhspClientService {
 	constructor(private readonly httpService: HttpService, private readonly configService: ConfigService) {}
@@ -15,7 +16,7 @@ export class OhspClientService {
 		const AEFI_PHONE_NUMBER = this.configService.get('AEFI_PHONE_NUMBER');
 
 		const request = this.httpService.get<IDhis2TrackedEntityInstance>(
-			`/trackedEntityInstances.json?ou=${AEFI_VACCINES_OU}&ouMode=${AEFI_VACCINES_OU_MODE}&filter=${AEFI_PHONE_NUMBER}:EQ:${phoneNumber}&fields=enrollments[events],attributes`,
+			`/trackedEntityInstances.json?ou=${AEFI_VACCINES_OU}&ouMode=${AEFI_VACCINES_OU_MODE}&filter=${AEFI_PHONE_NUMBER}:EQ:${phoneNumber}&fields=enrollments[events],attributes,orgUnit,trackedEntityInstance`,
 		);
 		const response = await lastValueFrom(request);
 		return response.data;
@@ -27,7 +28,7 @@ export class OhspClientService {
 		const AEFI_EPI_NUMBER_FILTER = this.configService.get('AEFI_EPI_NUMBER_FILTER');
 
 		const request = this.httpService.get<IDhis2TrackedEntityInstance>(
-			`/trackedEntityInstances.json?ou=${AEFI_VACCINES_OU}&ouMode=${AEFI_VACCINES_OU_MODE}&filter=${AEFI_EPI_NUMBER_FILTER}:EQ:${epiNumber}&fields=enrollments[events],attributes`,
+			`/trackedEntityInstances.json?ou=${AEFI_VACCINES_OU}&ouMode=${AEFI_VACCINES_OU_MODE}&filter=${AEFI_EPI_NUMBER_FILTER}:EQ:${epiNumber}&fields=enrollments[events],attributes,orgUnit,trackedEntityInstance`,
 		);
 		const response = await lastValueFrom(request);
 		return response.data;
@@ -50,5 +51,59 @@ export class OhspClientService {
 		const request = this.httpService.get<Dhis2Option>(`/options/${vaccineId}.json`);
 		const response = await lastValueFrom(request);
 		return response.data;
+	}
+	async getOptionsSetValues(optionSetId: string) {
+		const request = this.httpService.get<Dhis2OptionsSet>(`/optionSets/${optionSetId}.json`);
+		const response = await lastValueFrom(request);
+		return response.data;
+	}
+
+	async getOption(optionId: string) {
+		const request = this.httpService.get<Dhis2Option>(`/options/${optionId}.json`);
+		const response = await lastValueFrom(request);
+		return response.data;
+	}
+
+	async createVaccineEvent(createAefiDto: CreateAefiDto) {
+		const AEFI_VACCINE_STAGE = this.configService.get<string>('AEFI_VACCINE_STAGE');
+		const AEFI_SEVERITY = this.configService.get<string>('AEFI_SEVERITY');
+		const trackedEntityInstance = createAefiDto.trackedEntityInstanceId;
+		const payload: CreateNewDhis2EventDto = {
+			program: createAefiDto.programId,
+			programStage: AEFI_VACCINE_STAGE,
+			trackedEntityInstance: createAefiDto.trackedEntityInstanceId,
+			orgUnit: createAefiDto.orgUnitId,
+			eventDate: moment().format('YYYY-MM-DD'),
+			status: 'COMPLETE',
+			completedDate: moment().format('YYYY-MM-DD'),
+			dataValues: [...createAefiDto.aefiSideEffects, { dataElement: AEFI_SEVERITY, value: createAefiDto.aefiSeverityId }],
+		};
+
+		try {
+			await this.httpService.post(`/events.json?programStage=${AEFI_VACCINE_STAGE}&trackedEntityInstance=${trackedEntityInstance}`, payload);
+			return true;
+		} catch (error) {
+			throw new ServiceUnavailableException();
+		}
+	}
+
+	async getDhis2Resource<T>(url: string) {
+		try {
+			const request = await this.httpService.get<T>(url);
+			const response = await lastValueFrom(request);
+			return response.data;
+		} catch (error) {
+			throw new ServiceUnavailableException();
+		}
+	}
+
+	async createDhis2Resource<Payload, Return>(url: string, payload: Payload): Promise<Return> {
+		try {
+			const request = await this.httpService.post<Return>(url, payload);
+			const response = await lastValueFrom(request);
+			return response.data;
+		} catch (error) {
+			throw new ServiceUnavailableException();
+		}
 	}
 }
--- src/vaccine/vaccine.service.ts
+++ src/vaccine/vaccine.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { OhspClientService } from 'src/ohsp/ohsp-client.service';
+import { Dhis2Constant, Dhis2Constants } from '../common/types';
+import { OhspClientService } from '../ohsp/ohsp-client.service';
 import { VaccineTypeDto } from '../common/dtos/vaccine-type.dto';
 
 @Injectable()
@@ -8,10 +9,26 @@ export class VaccineService {
 
 	async getAllVaccineTypes(): Promise<VaccineTypeDto[]> {
 		const vaccinesfromOhsp = await this.ohspClient.getAllVaccines();
+		const vaccineDosages = await this.getDosageInformation();
 
 		return vaccinesfromOhsp.map((vaccine) => ({
 			categoryOptionId: vaccine.id,
 			vaccineName: vaccine.displayName,
+			//TODO: Matching is a little brittle, consider replacing with a regex.
+			numberOfDosages: vaccineDosages.find((dosageInfo) => dosageInfo.name.includes(vaccine.displayName) && dosageInfo.numberOfDoses < 10)
+				?.numberOfDoses,
+		}));
+	}
+
+	private async getDosageInformation() {
+		const allConstantsFromDhis2 = await this.ohspClient.getDhis2Resource<Dhis2Constants>('/constants.json');
+		const dosageInformationFromDhis2 = await Promise.all(
+			allConstantsFromDhis2.constants.map((constant) => this.ohspClient.getDhis2Resource<Dhis2Constant>(`/constants/${constant.id}.json`)),
+		);
+
+		return dosageInformationFromDhis2.map((dosage) => ({
+			name: dosage.displayFormName,
+			numberOfDoses: Math.floor(dosage.value),
 		}));
 	}
 }
