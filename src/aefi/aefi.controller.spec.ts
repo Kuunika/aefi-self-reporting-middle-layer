48b7b118a09feb2d0a5c071ea4037858d09277a5
Brett Larry Onions <46473797+brett-onions@users.noreply.github.com>
2022-03-08 13:20:10 +0200

    linting

--- src/aefi/aefi.controller.spec.ts
+++ src/aefi/aefi.controller.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AefiController } from './aefi.controller';
 
 describe('AefiController', () => {
-  let controller: AefiController;
+	let controller: AefiController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AefiController],
-    }).compile();
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [AefiController],
+		}).compile();
 
-    controller = module.get<AefiController>(AefiController);
-  });
+		controller = module.get<AefiController>(AefiController);
+	});
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+	it('should be defined', () => {
+		expect(controller).toBeDefined();
+	});
 });
--- src/common/exceptions/TrackedEntityInstanceNotFound.exception.ts
+++ src/common/exceptions/TrackedEntityInstanceNotFound.exception.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class TrackedEntityInstanceNotFoundException extends HttpException {
-  constructor() {
-    super('Tracked Entity Instance Not Found', HttpStatus.NOT_FOUND);
-  }
+	constructor() {
+		super('Tracked Entity Instance Not Found', HttpStatus.NOT_FOUND);
+	}
 }
