56e763464eedac014e6d0d9bee76090dd8b9071e
Brett Larry Onions <46473797+brett-onions@users.noreply.github.com>
2022-03-10 11:50:26 +0200

    clean up

--- src/common/pipes/valiidatePatientSideEffectRecord/validate-patient-side-effect-record.pipe.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ValidatePatientSideEffectRecordPipe } from './validate-patient-side-effect-record.pipe';
-
-describe('ValidatePatientSideEffectRecordPipe', () => {
-  it('should be defined', () => {
-    expect(new ValidatePatientSideEffectRecordPipe()).toBeDefined();
-  });
-});
