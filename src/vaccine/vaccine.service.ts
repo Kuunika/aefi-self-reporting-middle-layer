6e979990ebd87d41c6ffce489f368f9166ade6db
Brett Larry Onions <46473797+brett-onions@users.noreply.github.com>
2022-04-22 12:19:29 +0200

    Bug fix: getting vaccine types

--- src/vaccine/vaccine.service.ts
+++ src/vaccine/vaccine.service.ts
@@ -1,14 +1,19 @@
 import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
-import { Dhis2Constant, Dhis2Constants } from '../common/types';
+import { Dhis2Constant, Dhis2Constants, Dhis2Option, Dhis2OptionSet } from '../common/types';
 import { OhspClientService } from '../ohsp/ohsp-client.service';
 import { VaccineTypeDto } from '../common/dtos/vaccine-type.dto';
 import { Cache } from 'cache-manager';
 import { DosageInformation } from './interface/dosage-information';
 import { VACCINE_DOSAGES, VACCINE_TYPES } from 'src/common/constants';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class VaccineService {
-	constructor(private readonly ohspClient: OhspClientService, @Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+	constructor(
+		private readonly ohspClient: OhspClientService,
+		private readonly config: ConfigService,
+		@Inject(CACHE_MANAGER) private cacheManager: Cache,
+	) {}
 
 	async getAllVaccineTypes(): Promise<VaccineTypeDto[]> {
 		const fromCache = await this.cacheManager.get<VaccineTypeDto[]>(VACCINE_TYPES);
@@ -16,8 +21,10 @@ export class VaccineService {
 		if (fromCache) {
 			return fromCache;
 		}
+		const AEFI_VACCINE_DHIS_OPTIONS = this.config.get('AEFI_VACCINE_OPTIONS_SET');
+		const request = await this.ohspClient.getDhis2Resource<Dhis2OptionSet>(`/optionSets/${AEFI_VACCINE_DHIS_OPTIONS}.json`);
 
-		const vaccinesFromOhsp = await this.ohspClient.getAllVaccines();
+		const vaccinesFromOhsp = await Promise.all(request.options.map((vaccine) => this.getVaccineName(vaccine.id)));
 		const vaccineDosages = await this.getDosageInformation();
 
 		const vaccineTypes = vaccinesFromOhsp.map((vaccine) => ({
@@ -31,6 +38,11 @@ export class VaccineService {
 
 		this.cacheManager.set(VACCINE_TYPES, vaccineTypes, { ttl: 40_000 });
 		return vaccineTypes;
+	}
+	//TODO: Find Better Name
+	private async getVaccineName(vaccineId: string) {
+		const response = await this.ohspClient.getDhis2Resource<Dhis2Option>(`/options/${vaccineId}.json`);
+		return response;
 	}
 
 	private async getDosageInformation(): Promise<DosageInformation[]> {
